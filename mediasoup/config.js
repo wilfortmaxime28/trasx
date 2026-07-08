4d50d601e85c49bce4eb15ad41d81b38d4872101
Maxime Wilfort <wilfortmaxime28@gmail.com>
2026-07-08 19:59:48 -0400

    fix(live): dynamically detect local IP for local testing and use MEDIASOUP_ANNOUNCED_IP for production

--- mediasoup/config.js
+++ mediasoup/config.js
@@ -1,3 +1,19 @@
+const os = require('os');
+
+function getLocalIp() {
+  const interfaces = os.networkInterfaces();
+  for (const devName in interfaces) {
+    const iface = interfaces[devName];
+    for (let i = 0; i < iface.length; i++) {
+      const alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return '127.0.0.1';
+}
+
 module.exports = {
   // Configuration des workers
   workerSettings: {
@@ -39,7 +55,7 @@ module.exports = {
     listenIps: [
       {
         ip: '0.0.0.0',
-        announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP || '127.0.0.1'
+        announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP || getLocalIp()
       }
     ],
     initialAvailableOutgoingBitrate: 1000000,
